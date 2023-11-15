9722974cb3e9935cc54d72311e430b3b657b8abe
Sai Sree <kottam.saisree@gmail.com>
2023-11-14 20:23:31 -0600

    Add basicButtons

--- README.md
+++ README.md
@@ -4,3 +4,5 @@ This is a basic calculator with add, subtract, multiply and divide functionaliti
 
 Steps:
 1. Implemented the functions initially and tested with console output.
+2. Added operate function with parameters num1, num2, operand.
+
--- script.js
+++ script.js
@@ -1,3 +1,25 @@
+const num1 = 2;
+const num2 =7;
+const op = "+";
+
+operate(num1,num2,op);
+
+function operate(num1,num2,op){
+    switch(op){
+        case "+" : console.log(add(num1,num2));
+            break;
+        case "-" : console.log(sub(num1,num2));
+            break;
+        case "*" : console.log(mul(num1,num2));
+            break;
+        case "/" : console.log(div(num1,num2));
+            break;
+        default : "Invalid";
+        
+    }
+}
+
+
 function add(a,b){
     return a+b;
 }
@@ -14,7 +36,7 @@ function div(a,b){
     return a/b;
 }
 
-console.log(add(2,7));
-console.log(sub(2,7));
-console.log(mul(2,7));
-console.log(div(2,7));
+// console.log(add(2,7));
+// console.log(sub(2,7));
+// console.log(mul(2,7));
+// console.log(div(2,7));
