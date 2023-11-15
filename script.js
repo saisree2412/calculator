edd3fc01d64fd0c12b834a261db5796fe3271cb7
Sai Sree <kottam.saisree@gmail.com>
2023-11-14 20:05:35 -0600

    Add basicFunctions

---
+++ script.js
@@ -0,0 +1,20 @@
+function add(a,b){
+    return a+b;
+}
+
+function sub(a,b){
+    return a-b;
+}
+
+function mul(a,b){
+    return a*b;
+}
+
+function div(a,b){
+    return a/b;
+}
+
+console.log(add(2,7));
+console.log(sub(2,7));
+console.log(mul(2,7));
+console.log(div(2,7));
